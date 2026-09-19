bd11f8dce710fc1ae31c27b8ca3ac8934dc6c330
BUSAN SAFE GUIDE <luvlymjmj@gmail.com>
2026-09-19 16:31:43 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,48 +1,23 @@
-const CACHE_NAME = "h-link-v1";
-
-const APP_FILES = [
-  "/h-link/",
-  "/h-link/index.html",
-  "/h-link/manifest.json",
-  "/h-link/icon-512.png"
-];
+const CACHE_NAME = "h-link-v2";
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_FILES))
-  );
-
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => caches.delete(key))
+      );
+    })
   );
 
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-
-        caches.open(CACHE_NAME)
-          .then(cache => cache.put(event.request, copy));
-
-        return response;
-      })
-      .catch(() => caches.match(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
